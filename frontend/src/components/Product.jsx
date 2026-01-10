4013405072cf425f97a16a9ecb259122953becc2
webmekanic <aniebovictor001@gmail.com>
2026-01-10 12:54:34 +0100

    Feat: add product rating component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,7 +1,8 @@
-import { Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
@@ -10,16 +11,20 @@ const Product = ({product}) => {
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             {product.name}
           </Card.Title>
         </Link>
-        <Card.Text as="h3">
-          ${product.price}
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
